f1f20e4b2867ba219ca6101be7198c33630d7075
samhowes <sam@samhowes.com>
2021-04-03 19:33:23 -0400

    added new project and build.targets

---
+++ HelloNuGet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloNuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
