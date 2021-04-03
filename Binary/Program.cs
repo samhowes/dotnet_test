d1d19d13129052727fe965ec42daed04396df2c5
samhowes <sam@samhowes.com>
2021-04-03 19:12:11 -0400

    a commit

---
+++ Binary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TransitiveTransitiveLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TransitiveTransitiveLib
+{
+    public class Class1
+    {
+    }
+}
